c3b0d11816d9b537b57cd8038d8269b75c95034f
Michele Gubian <michele.gubian@gmail.com>
2023-09-17 15:07:17 +0200

    fixed out of range condition in t2treg

--- R/t2treg.R
+++ R/t2treg.R
@@ -3,6 +3,7 @@
 #' An auxiliary thin wrapper to `fda::eval.fd()`.
 #' Given an already computed inverse time warping function `hinv`,
 #' map original time points `argvals` to their registered position `hinv`(`argvals`).
+#' Values outside the range of `hinv` are mapped to `NA`s.
 #'
 #' @param argvals A vector of time points
 #' @param hinv A `fda::fd` object containing only one curve
@@ -12,5 +13,8 @@
 #'
 #' @examples
 t2treg <- function(argvals, hinv) {
-  return(fda::eval.fd(argvals, hinv) %>% as.numeric())
+  inRange <- dplyr::between(argvals, hinv$basis$rangeval[1], hinv$basis$rangeval[2])
+  treg <- rep(NA, times = length(argvals))
+  treg[inRange] <- fda::eval.fd(argvals[inRange], hinv) %>% as.numeric()
+  return(treg)
 }
